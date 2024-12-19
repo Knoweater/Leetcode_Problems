24a3c60b6471f1f88ba511bfd135390896da6648
unknown <zhenyakucher10@gmail.com>
2024-12-19 16:05:35 +0200

    Files

--- Leetcode_Prob/Leetcode_Prob/PlusOne1.cpp
+++ Leetcode_Prob/Leetcode_Prob/PlusOne1.cpp
@@ -1,51 +1,51 @@
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//using namespace std;
-//
-//class Solution {
-//public:
-//    vector<int> plusOne(vector<int>& digits) {
-//        string str;
-//        int number = 0;
-//
-//        for (int iter : digits) {
-//            str += to_string(iter);
-//        }
-//
-//        number = stoi(str);
-//        number++;
-//        
-//     
-//        if (to_string(number).length() > str.length()) {
-//            digits.resize(digits.size() + 1);
-//        }
-//
-//        for (int i = digits.size() - 1; i >= 0; i--) {
-//            digits[i] = number % 10;
-//            number /= 10;
-//        }
-//
-//
-//        for (int i = 0; i < digits.size(); i++) {
-//            cout << digits[i];
-//        }
-//        
-//        return digits;
-//    }
-//};
-//
-//int main() {
-//
-//    Solution Sol;
-//
-//
-//    vector<int> My_vec = { 9,8,7,6,5,4,3,2,1,0 };
-//   
-//    Sol.plusOne(My_vec);
-//
-//
-//   
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        string str;
+        int number = 0;
+
+        for (int iter : digits) {
+            str += to_string(iter);
+        }
+
+        number = stoi(str);
+        number++;
+        
+     
+        if (to_string(number).length() > str.length()) {
+            digits.resize(digits.size() + 1);
+        }
+
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+
+
+        for (int i = 0; i < digits.size(); i++) {
+            cout << digits[i];
+        }
+        
+        return digits;
+    }
+};
+
+int main() {
+
+    Solution Sol;
+
+
+    vector<int> My_vec = { 9 };
+   
+    Sol.plusOne(My_vec);
+
+
+   
+
+	return 0;
+}
