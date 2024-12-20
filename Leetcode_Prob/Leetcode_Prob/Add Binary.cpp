f4767caf2ee70fe70c247a2e336977750cc89faf
unknown <zhenyakucher10@gmail.com>
2024-12-20 12:39:51 +0200

    New file

---
+++ Leetcode_Prob/Leetcode_Prob/Add Binary.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+
+
+
+    }
+};
+
+int main() {
+
+    Solution Sol;
+
+    string first = "11";
+    string second = "1";
+
+
+
+    cout << Sol.addBinary(first, second);
+
+	return 0;
+}
